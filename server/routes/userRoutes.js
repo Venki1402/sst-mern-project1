7b4d02f3c1d20948d8a4c02844ae73ab30de5401
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-07-10 11:24:27 +0530

    Implemented register route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
   try {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res.send({
+        success: false,
+        message: "User already exists"
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword
+
     const newUser = new User(req.body);
     await newUser.save();
     res.send("User registered");
